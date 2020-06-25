5d5c983d4ecbfce063770188d0f31e994f4b2b31
Felipe Guerra <fguerra.ag@gmail.com>
2020-06-24 23:07:14 -0400

    final desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -58,8 +58,109 @@ SELECT M.nombre FROM peliculas AS M INNER JOIN reparto AS A ON M.id = A.pelicula
 --  Indiana Jones y el templo maldito
 -- (8 filas)
 
+--6. Listar los 10 directores mas populares, indicando su nombre y cuántas películas aparecen en el top 100.
+SELECT director, count(*) AS N_peliculas FROM peliculas GROUP BY director ORDER BY N_peliculas DESC LIMIT 10;
+-- director       | n_peliculas 
+-- ----------------------+-------------
+--  Steven Spielberg     |           9
+--  Francis Ford Coppola |           4
+--  Peter Jackson        |           4
+--  Quentin Tarantino    |           3
+--  Martin Scorsese      |           3
+--  Robert Zemeckis      |           3
+--  James Cameron        |           3
+--  Ridley Scott         |           3
+--  George Lucas         |           3
+--  Frank Darabont       |           2
+-- (10 filas)
 
+--7. Indicar cuantos actores distintos hay 
+SELECT DISTINCT(nombre_reparto) FROM reparto;
+--ME SALIERON MUCHOS.... SOLO DEJE EL FINAL
+-- Lupe Ontiveros
+--  John Turturro
+--  Claude Rains
+--  Stephen Rea
+--  Winona Ryder
+--  Scatman Crothers
+--  Uma Thurman
+--  John Gavin
+--  Robert Shaw
+--  John Travolta
+--  John Cazale
+--  Shane West
+--  Tyrese Gibson
+--  Jean Reno
+--  Max von Sydow
+--  Sally Field
+--  Ewen Bremner
+--  André Morell
+--  Eric Stoltz
+--  Ngc Le
+--  Silas Carson
+--  Stellan Skarsgård
+--  Peter Greene
+--  Dave Prowse
+--  Cotter Smith
+-- (831 filas)
 
+--8. Indicar las películas estrenadas entre los años 1990 y 1999 (ambos incluidos) ordenadas por título de manera ascendente.
+SELECT año, nombre FROM peliculas WHERE año BETWEEN 1990 AND 1999 ORDER BY año ASC;
+-- año  |                   nombre                   
+-- ------+--------------------------------------------
+--  1990 | Uno de los nuestros
+--  1990 | Eduardo Manostijeras
+--  1990 | El Padrino. Parte III
+--  1991 | Terminator 2: el juicio final
+--  1991 | El silencio de los corderos
+--  1993 | La lista de Schindler
+--  1993 | Parque Jurásico
+--  1993 | Pesadilla antes de navidad
+--  1994 | Forest Gump
+--  1994 | Cadena perpetua
+--  1994 | Pulp Fiction
+--  1994 | Entrevista con el vampiro
+--  1994 | El profesional
+--  1995 | Seven
+--  1995 | Toy Story
+--  1995 | Braveheart
+--  1996 | Trainspotting
+--  1997 | Titanic
+--  1997 | Hombres de negro
+--  1997 | Mejor... imposible
+--  1998 | American History X
+--  1998 | Salvar al soldado Ryan
+--  1998 | El show de Truman
+--  1999 | Toy Story 2
+--  1999 | La milla verde
+
+--10. Listar los actores de la película más nueva
+SELECT nombre_reparto FROM reparto WHERE peliculas_id IN (SELECT id FROM peliculas ORDER BYaño DESC LIMIT 1);
+-- nombre_reparto    
+-- ----------------------
+--  Christian Bale
+--  Heath Ledger
+--  Aaron Eckhart
+--  Michael Caine
+--  Gary Oldman
+--  Maggie Gyllenhaal
+--  Morgan Freeman
+--  Eric Roberts
+--  Cillian Murphy
+--  Anthony Michael Hall
+--  Michael Jai White
+--  William Fichtner
+--  Edison Chen
+--  Monique Curnen
+--  Chin Han
+--  Nestor Carbonell
+--  Ritchie Coster
+--  Keith Szarabajka
+--  Colin McFarlane
+--  Melinda McGraw
+--  Nathan Gamble
+--  Michael Vieau
+-- (22 filas)
 
 
 
