e8c315eba7477f2c92a05cedaf0b7462e295ac75
Felipe Guerra <fguerra.ag@gmail.com>
2020-06-26 15:09:36 -0400

    desafio3

--- desafio3/caso2.sql
+++ desafio3/caso2.sql
@@ -1,38 +1,33 @@
---CREATE DATABASE desafio3s;
-CREATE TABLE "cursos" ( "id" SERIAL PRIMARY KEY, "nombre" VARCHAR);
--- SELECT * FROM cursos;
+CREATE TABLE curso(id SERIAL,nombre VARCHAR,PRIMARY KEY (id));
+-- SELECT * FROM CURSO;
 --  id | nombre 
 -- ----+--------
 -- (0 filas)
 
-CREATE TABLE "alumnos" ( "id" SERIAL, "nombre_alumnos" VARCHAR, "cursos_id" INT, "rut" INT);
--- SELECT * FROM alumnos;
---  id | nombre_alumno | cursos_id | rut 
--- ----+---------------+-----------+-----
+
+CREATE TABLE alumno(id SERIAL UNIQUE,nombre_alumno VARCHAR,curso_id INT,rut INT,FOREIGN KEY(curso_id) REFERENCES curso(id));
+-- SELECT * FROM ALUMNO;
+--  id | nombre_alumno | curso_id | rut 
+-- ----+---------------+----------+-----
 -- (0 filas)
 
-CREATE TABLE "departamentos" ( "id" SERIAL PRIMARY KEY, "nombre_departamento" VARCHAR);
--- SELECT * FROM departamentos;
+CREATE TABLE departamento(id SERIAL PRIMARY KEY,nombre_departamento VARCHAR);
+-- SELECT * FROM departamento;
 --  id | nombre_departamento 
 -- ----+---------------------
 -- (0 filas)
 
-CREATE TABLE "profesores" ( "id" SERIAL PRIMARY KEY, "nombre_profesor" VARCHAR, "departamentos_id" INT);
--- SELECT * FROM profesores;
+CREATE TABLE profesor(id SERIAL PRIMARY KEY,nombre_profesor VARCHAR,departamento_id INT,OREIGN KEY(departamento_id) REFERENCES departamento);
+-- SELECT * FROM profesor;
 --  id | nombre_profesor | departamento_id 
 -- ----+-----------------+-----------------
 -- (0 filas)
 
-CREATE TABLE "pruebas_alumnos" ( "id" SERIAL PRIMARY KEY, "nota" FLOAT, "score" FLOAT, "alumnos_id" INT, "profesores_id" INT);
--- SELECT * FROM pruebas_alumnos;
---  id | nota | score | alumnos_id | profesores_id 
--- ----+------+-------+------------+---------------
+
+CREATE TABLE prueba_alumno (id SERIAL PRIMARY KEY,nota FLOAT,score FLOAT,alumno_id INT,profesor_id INT,FOREIGN KEY (alumno_id) REFERENCES alumno(id),
+FOREIGN KEY (profesor_id) REFERENCES profesor(id)
+);
+-- SELECT * FROM prueba_alumno;
+--  id | nota | score | alumno_id | profesor_id 
+-- ----+------+-------+-----------+-------------
 -- (0 filas)
-
-ALTER TABLE "alumnos" ADD FOREIGN KEY ("cursos_id") REFERENCES "cursos" ("id");
-
-ALTER TABLE "profesores" ADD FOREIGN KEY ("departamentos_id") REFERENCES "departamentos" ("id");
-
-ALTER TABLE "pruebas_alumnos" ADD FOREIGN KEY ("alumnos_id") REFERENCES "alumnos" ("id");
-
-ALTER TABLE "pruebas_alumnos" ADD FOREIGN KEY ("profesores_id") REFERENCES "profesores" ("id");
